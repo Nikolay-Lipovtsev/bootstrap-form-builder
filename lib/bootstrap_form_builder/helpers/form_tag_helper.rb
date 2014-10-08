690654d044958eb5cfa2fabd889b8a3da8d305c1
Nikolay-Lipovtsev <n.lipovtsev@gmail.com>
2014-10-08 20:40:03 +0400

    Creating descriptions for grid system module

--- lib/bootstrap_form_builder/helpers/form_tag_helper.rb
+++ lib/bootstrap_form_builder/helpers/form_tag_helper.rb
@@ -5,6 +5,7 @@ module BootstrapFormBuilder
     module FormTagHelper
       include BootstrapFormBuilder::Helpers::Blocks::ButtonTag 
       
+      # ==== Options
       # :tag
       # :style
       # :size
@@ -12,6 +13,8 @@ module BootstrapFormBuilder
       # :active
       # :disabled
       # :col
+      # :offset_col
+      # :row_disable
       def button_tag(content_or_options = nil, options = nil, &block)
         if content_or_options.is_a? Hash
           options = content_or_options
@@ -20,17 +23,17 @@ module BootstrapFormBuilder
           options[:content] = content_or_options
         end
         
-        button_block(options[:col]) { super options, &block }
+        button_block(options[:col], options) { super options, &block }
       end
       
       def button_link_tag(name = nil, options = nil, html_options = nil, &block)
-        button_block(html_options[:col]) { super name, options, html_options, &block }
+        button_block(html_options[:col], html_options) { super name, options, html_options, &block }
       end
       
       private
       
-      def button_block(col = nil)
-        col ? bootstrap_row_with_col(col) { yield } : yield
+      def button_block(col = nil, options = {})
+        col ? bootstrap_row_with_col(col, options.slice(:row_disable, :offset_col)) { yield } : yield
       end
     end
   end
--- lib/bootstrap_form_builder/helpers/grid_system_helper.rb
+++ lib/bootstrap_form_builder/helpers/grid_system_helper.rb
@@ -3,13 +3,47 @@ module BootstrapFormBuilder
     # Provides a number of methods for creating a simple Bootstrap blocks
     module GridSystemHelper
       
+      # Creates a HTML div block with bootstrap row class.
+      #
+      # === Options
+      # You can use only symbols for the attribute names.
+      #
+      # <tt>:row_disable</tt> if set to true, the content will build without bootstrap row div block,
+      # it will return empty content.
+      #
+      # === Examples
+      # bootstrap_row { "Test" }
+      # # => <div class="row">"Test"</div>
+      #
+      # bootstrap_row(class: "foo") { "Test" }
+      # # => <div class="row foo">"Test"</div>
+      #
+      # bootstrap_row(row_disable: true) { "Test" }
+      # # => "Test"
       def bootstrap_row(options = {})
         options[:class] = ["row", options[:class]].compact.join(" ")
-        content_tag(:div, class: options[:class]) { yield }
+        options[:row_disable] ? yield : content_tag(:div, class: options[:class]) { yield }
       end
       
       alias :control_group :bootstrap_row
       
+      # Creates a HTML div block with bootstrap grid column class.
+      #
+      # === Options
+      # You can use only symbols for the attribute names.
+      #
+      # <tt>:grid_system</tt> if the set value from bootstrap grid system you can chenge default value "sm" 
+      # to "xs", "sm", "md" or "lg".
+      # 
+      # <tt>:offset_col</tt> if the set value in the range from 1 to 12 it generate bootstrap offset class 
+      # in HTML div block with bootstrap grid column.
+      #
+      # === Examples
+      # bootstrap_col { "Test" }
+      # # => <div class="col-sm-12">"Test"</div>
+      #
+      # bootstrap_col(6) { "Test" }
+      # # => <div class="col-sm-6">"Test"</div>
       def bootstrap_col(col = nil, options = {})
         options[:class] = [grid_system_class(options[:grid_system], col), 
                           grid_system_offset_class(options[:grid_system], options[:offset_col]), 
@@ -21,19 +55,19 @@ module BootstrapFormBuilder
         bootstrap_row(options) { bootstrap_col(col, options.slice(:grid_system, :offset_col)) { yield }}
       end
       
-      def grid_system_class(grid_system = default_grid_system, col = default_control_col)
-        "col-#{(grid_system || default_grid_system).to_s}-#{col || default_control_col}"
+      def grid_system_class(grid_system = default_grid_system, col = default_col)
+        "col-#{(grid_system || default_grid_system).to_s}-#{col || default_col}"
       end
 
       def grid_system_offset_class(grid_system, col)
         "col-#{(grid_system || default_grid_system).to_s}-offset-#{col}" if col
       end
-
+      
       def default_horizontal_label_col
         2
       end
 
-      def default_control_col
+      def default_col
         12
       end
 
